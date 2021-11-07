3c035dc705913f4e0d0c555d409582895cebc6e1
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-11-07 18:09:44 -0300

    ensure CreateUserUseCase not be able to create new users when email is already taken

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,8 +10,16 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
+    const userEmailAlreadyExists = this.usersRepository.findByEmail(email);
+
+    if (userEmailAlreadyExists) {
+      throw new Error(`User already exists`);
+    }
+
     this.usersRepository.create({ name, email });
+
     const user = this.usersRepository.findByEmail(email);
+
     return user;
   }
 }
